8845355c07f690424da667d69ffe9d1de39336f9
lazy-sky <1cielo@naver.com>
2022-12-24 23:28:33 +0900

    Add a problem
    - average

---
+++ problems/average/average.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {number[]} numbers - 숫자로 이루어진 배열
+ * @returns {numbers} average - numbers의 평균값
+ */
+function solution(numbers) {
+  // 코드 작성
+  let answer;
+  return answer;
+}
+
+export default solution;

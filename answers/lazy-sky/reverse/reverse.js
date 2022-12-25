fc369cdb0043a5c56530200fe3489b8390ab075d
lazy-sky <1cielo@naver.com>
2022-12-25 15:57:31 +0900

    Add a problem with an answer
    - reverse

---
+++ answers/lazy-sky/reverse/reverse.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {any[]} list - 배열
+ * @returns {any[]} average - list를 거꾸로 뒤집은 배열
+ */
+function solution(list) {
+  list.reverse();
+  return list;
+}
+
+export default solution;

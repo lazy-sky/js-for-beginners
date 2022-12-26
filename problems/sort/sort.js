b83c8d1d69b8924a0b6ff591862f4651175cd514
lazy-sky <1cielo@naver.com>
2022-12-26 14:54:48 +0900

    Add a problem
    - sort

---
+++ problems/sort/sort.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {number[]} numbers - 숫자 배열
+ * @returns {number[]} sorted - numbers를 내림차순으로 정렬한 배열
+ */
+function solution(numbers) {
+  // 코드 작성
+  let answer;
+  return answer;
+}
+
+export default solution;
